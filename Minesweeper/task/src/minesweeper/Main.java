d5ae709190711462176967fabd96fd0d012344a2
Nick Christiny <nchristiny@users.noreply.github.com>
2020-11-28 19:20:20 -0600

    Add board and RND mine placement

--- Minesweeper/task/src/minesweeper/Main.java
+++ Minesweeper/task/src/minesweeper/Main.java
@@ -1,7 +1,45 @@
 package minesweeper;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
+    static Scanner sc = new Scanner(System.in);
+    static String[][] board = new String[9][9];
+    static int mines;
+
+    public static void showBoard() {
+        //show board
+        for (int i = 0; i <= 8; i++) {
+            for (int j = 0; j <= 8; j++) {
+                System.out.print(board[i][j]);
+            }
+            System.out.println();
+        }
+    }
+
     public static void main(String[] args) {
-        // write your code here
+        System.out.print("How many mines do you want on the field? > ");
+        mines = sc.nextInt();
+        int count = mines;
+        Random rand = new Random();
+        // set the mines
+        while (count > 0) {
+            // random coords for each mine
+            int p = rand.nextInt(8);
+            int q = rand.nextInt(8);
+            if (board[p][q] != "X") {
+                board[p][q] = "X";
+                count--;
+            }
+        }
+        // fill the rest in
+        for (int i = 0; i <= 8; i++) {
+            for (int j = 0; j <= 8; j++) {
+                if (board[i][j] != "X")
+                    board[i][j] = ".";
+            }
+        }
+        showBoard();
     }
 }
