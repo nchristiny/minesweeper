ffbb695d2a4c0062d183f86a7ceced7c97fffe9b
Nick Christiny <nchristiny@users.noreply.github.com>
2021-01-09 21:31:06 -0600

    Add border and row labels

--- Minesweeper/task/src/minesweeper/Main.java
+++ Minesweeper/task/src/minesweeper/Main.java
@@ -6,44 +6,52 @@ import java.util.Scanner;
 public class Main {
     // TODO need to accept user defined size of m x n field
     static Scanner sc = new Scanner(System.in);
+    // TODO use char[][]
     static String[][] board = new String[9][9];
     static int mines;
 
     public static void showBoard() {
         /* show board */
+        System.out.println("|123456789|");
+        System.out.println("-|---------|-");
         for (int i = 0; i <= 8; i++) {
             for (int j = 0; j <= 8; j++) {
+                if (j == 0)
+                    System.out.print((i + 1) + "|");
                 System.out.print(board[i][j]);
+                if (j == 8)
+                    System.out.print("|");
             }
             System.out.println();
         }
+        System.out.println("-|---------|-");
     }
 
     public static void main(String[] args) {
         System.out.print("How many mines do you want on the field? > ");
         mines = sc.nextInt();
-        int count = mines;
         Random rand = new Random();
+        int count;
         /* set the mines */
-        while (count > 0) {
+        while (mines > 0) {
             int p = rand.nextInt(8);
             int q = rand.nextInt(8);
             if (!board[p][q].equals("X")) {
                 board[p][q] = "X";
-                count--;
+                mines--;
             }
         }
         /* fill the rest in */
-        for (int i = 0; i <= 8; i++) {
-            for (int j = 0; j <= 8; j++) {
+        for (int j = 0; j <= 8; j++) {
+            for (int i = 0; i <= 8; i++) {
                 if (!board[i][j].equals("X"))
                     board[i][j] = ".";
             }
         }
 
         /* draw proximity */
-        for (int i = 0; i <= 8; i++) {
-            for (int j = 0; j <= 8; j++) {
+        for (int j = 0; j <= 8; j++) {
+            for (int i = 0; i <= 8; i++) {
                 count = 0;
                 if (!board[i][j].equals("X")) {
                     switch (i) {
